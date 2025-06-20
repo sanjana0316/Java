36213d62f77d564f6bf8b8657dc19d5db1fc1665
sanjana0316 <aerra2023@GMAIL.COM>
2025-06-20 09:48:21 -0500

    Create LogicalOperators.java

---
+++ LogicalOperators.java
@@ -0,0 +1,13 @@
+public class LogicalOperators
+{
+    public static void main(String[] args) 
+    {
+        boolean x = true;
+        boolean y = false;
+
+        System.out.println("x && y: " + (x && y));
+        System.out.println("x || y: " + (x || y));
+        System.out.println("!x: " + (!x));
+        System.out.println("!y: " + (!y));
+    }
+}
