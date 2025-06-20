579f3e1aa0ece7c158d1b74e89ddb926628544e3
sanjana0316 <aerra2023@GMAIL.COM>
2025-06-20 09:41:52 -0500

    Create IncrementDecrement.java

---
+++ IncrementDecrement.java
@@ -0,0 +1,12 @@
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("Original: " + n);
+        System.out.println("++n (pre-increment): " + ++n);
+        System.out.println("n++ (post-increment): " + n++);
+        System.out.println("Now n is: " + n);
+        System.out.println("--n (pre-decrement): " + --n);
+        System.out.println("n-- (post-decrement): " + n--);
+        System.out.println("Now n is: " + n);
+    }
+}
