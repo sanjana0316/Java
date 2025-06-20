fda0df22d7d64497070366dff4b1c3fad4a9adb2
sanjana0316 <aerra2023@GMAIL.COM>
2025-06-20 07:50:06 -0500

    Create SwapFourWithoutTemp.java

---
+++ SwapFourWithoutTemp.java
@@ -0,0 +1,13 @@
+public class SwapFourWithoutTemp 
+{
+    public static void main(String[] args) 
+    {
+        int a = 1, b = 2, c = 3, d = 4;
+        a = a + b + c + d;
+        b = a - (b + c + d);
+        c = a - (b + c + d);
+        d = a - (b + c + d);
+        a = a - (b + c + d);
+        System.out.println("a=" + a + ", b=" + b + ", c=" + c + ", d=" + d);
+    }
+}
