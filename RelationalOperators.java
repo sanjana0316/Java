0d96bbdbde535eabdd8f213294f758d6c718c3fd
sanjana0316 <aerra2023@GMAIL.COM>
2025-06-20 09:47:28 -0500

    Create  RelationalOperators.java

---
+++ RelationalOperators.java
@@ -0,0 +1,15 @@
+public class RelationalOperators 
+{
+    public static void main(String[] args)
+   {
+        int a = 16;
+        int b = 5;
+
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a != b: " + (a != b));
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a >= b: " + (a >= b));
+        System.out.println("a <= b: " + (a <= b));
+    }
+}
