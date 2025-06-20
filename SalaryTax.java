e4c4fee712db1434e5d5f92d7a2c28201b874c92
sanjana0316 <aerra2023@GMAIL.COM>
2025-06-20 07:55:14 -0500

    Create SalaryTax.java

---
+++ SalaryTax.java
@@ -0,0 +1,11 @@
+public class SalaryTax 
+{
+    public static void main(String[] args) 
+    {
+        double salary = 85000;
+        double taxPercent = 20;
+
+        double taxAmount = salary * taxPercent / 100;
+        System.out.println("Tax Amount: ₹" + taxAmount);
+    }
+}
