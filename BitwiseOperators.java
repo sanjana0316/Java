0317ac4fc314978fda566bef754048b96e96b900
sanjana0316 <aerra2023@GMAIL.COM>
2025-06-20 09:51:07 -0500

    BitwiseOperators.java

---
+++ BitwiseOperators.java
@@ -0,0 +1,15 @@
+public class BitwiseOperators 
+{
+    public static void main(String[] args)
+   {
+        int a = 6;  
+        int b = 3;   
+
+        System.out.println("a & b: " + (a & b));   
+        System.out.println("a | b: " + (a | b));   
+        System.out.println("a ^ b: " + (a ^ b));   
+        System.out.println("~a: " + (~a));         
+        System.out.println("a << 1: " + (a << 1)); 
+        System.out.println("a >> 1: " + (a >> 1)); 
+    }
+}
