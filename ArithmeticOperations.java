6bee8235425e4d897851f712433845070abc0474
sanjana0316 <aerra2023@GMAIL.COM>
2025-06-20 09:06:58 -0500

    Create ArithmeticOperations.java

---
+++ ArithmeticOperations.java
@@ -0,0 +1,12 @@
+public class ArithmeticOperations {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        System.out.println("Addition of a and b is " + (a+b));
+        System.out.println("Substraction of a and b is " + (a-b));
+        System.out.println("Multiplication of a and b is " + a*b);
+        System.out.println("Division of a and b is " + a/b);
+        System.out.println("Modulus of a and b is " + a%b);
+    }
+}
