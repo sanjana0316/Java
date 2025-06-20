90ed0c009f8e85e45dbbba7c2502860955d46a97
sanjana0316 <aerra2023@GMAIL.COM>
2025-06-20 08:05:48 -0500

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,12 @@
+public class sum
+{
+    public static int sum(int a, int b) 
+    {
+        int result = a + b;  
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sum = " + sum(5, 7));
+    }
+}
